fa87e646c27d4a3681b02d0812b2cf626af9189b
illges <brown1ei.eb@gmail.com>
2025-02-11 22:08:32 -0500

    add main lua file

---
+++ d2ms.lua
@@ -0,0 +1,15 @@
+-- drums 2 midi synth...
+-- sampler, sequence,
+-- sound, something
+--
+-- v1.0.0
+--
+-- an augmented drumming script
+--
+-- developed by phillwilson
+--           and illges
+
+---@diagnostic disable: undefined-global, lowercase-global
+
+function init()
+end
